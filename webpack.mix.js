91f2db6d0807d70cf369f0a76f1b15582f2a4abe
Bruno <38008341+moevbiz@users.noreply.github.com>
2021-08-02 23:38:03 +0200

    fix header img not scaling correctly

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,18 +26,19 @@ mix
 
 
 // Production only
-if ( mix.inProduction() )
-{
 
-    // Purge our CSS
-    // mix minifies CSS & JS by default
-    // I prefer to add the `.min` suffix on the output files just for convention
-    mix.purgeCss({
-            content: ['site/**/*.njk'],
-            safelist: ['menu-visible', 'loaded', 'expanded', /^type-/, /^page-/, /[data-src]/],
-            extractorPattern: [/[^<>"'`\s]*[^<>"'`\s:]/g]
-        })
-        .minify('docs/assets/css/main.css')
-        .minify('docs/assets/js/build.js')
+// if ( mix.inProduction() )
+// {
 
-}
+//     // Purge our CSS
+//     // mix minifies CSS & JS by default
+//     // I prefer to add the `.min` suffix on the output files just for convention
+//     mix.purgeCss({
+//             content: ['site/**/*.njk'],
+//             safelist: ['menu-visible', 'loaded', 'expanded', /^type-/, /^page-/, /[data-src]/],
+//             extractorPattern: [/[^<>"'`\s]*[^<>"'`\s:]/g]
+//         })
+//         .minify('docs/assets/css/main.css')
+//         .minify('docs/assets/js/build.js')
+
+// }
